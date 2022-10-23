2fb77544414bba11584090ba3f05db3be71b8166
Stayna <staynaa.cs@gmail.com>
2022-10-23 15:34:41 -0400

    Part 1, Task 1
    Since there was a function that found the difference between two days already, I added a new toString to use that function and return a string to display the amount of days if there are.

--- lab1/countdown.java
+++ lab1/countdown.java
@@ -1,13 +1,19 @@
+//lab2 part 1.1
 import java.util.Calendar;
 import java.util.*;
 
 public class countdown {
     Calendar cur;
     Calendar lab;
-    countdown(){
+    Calendar day;
+    countdown(){ //for current day
         cur= Calendar.getInstance();
     }
-    countdown(int year, int month, int date){
+    countdown(int year,int month, int date,int hour, int min, int sec){ // for anyday
+        day= Calendar.getInstance();
+        day.set(year,month,date,hour,min,sec);
+    }
+    countdown(int year, int month, int date){ //for labs
         cur= Calendar.getInstance();
         lab= Calendar.getInstance();
         lab.set(year, month, date, 15, 30, 00); //3:30:00pm
@@ -16,7 +22,7 @@ public class countdown {
         return cur;
     }
     public int inBtw(Date current, Date due){ //difference between current date and due date
-        return (int)( (due.getTime() - current.getTime()) / (1000 * 60 * 60 * 24));
+        return (int)( (due.getTime() - current.getTime()) / (1000 * 60 * 60 * 24)); //milliseconds in a day: 86400000
     }
     public String toString(int labNum){
         int days= inBtw(cur.getTime(),lab.getTime());
@@ -25,8 +31,35 @@ public class countdown {
         else if(days==0) return "Lab "+ labNum + " is due in less than 24 hours";
         else return "Lab " + labNum + " is past due";
     }
+    //toString made for 1.1
+    public String toString(countdown d2){ //d2 is the future date
+        Date first= day.getTime(); //store objects' date in two variables 
+        Date second= d2.day.getTime();
+        int days= inBtw(first,second);
+        if (days>=0) return "There are "+ days +" days between "+ first +" and "+ second;
+        return "No days left between the two dates";
+    }
+
     //Main
     public static void main(String args[]){
+//inBtw function from lab1 already takes two dates and returns an int of the days between them
+        /*plan 1.1:
+            make two countdown objects
+            then call the toString, send 2nd object as parameter
+            have to string call inbtw 
+            reminder** months go from 0-11 instead of 1-12
+        */
+        countdown date1= new countdown(2023, 9, 30, 0, 0, 0); //10/30/22
+        countdown date2= new countdown(2023, 5, 13, 0, 0, 0); 
+        System.out.println(date1.toString(date2));
+
+
+
+
+
+
+/* LAB1:
+
         Calendar current= Calendar.getInstance(); //today
         countdown lab1= new countdown(2022, 8, 28); //lab 1 9/28
         countdown lab2= new countdown(2022, 9, 28); //lab 2 10/28
@@ -37,5 +70,7 @@ public class countdown {
         System.out.println(lab2.toString(2));
         System.out.println(lab3.toString(3));
         System.out.println(lab4.toString(4));
+
+*/
     }
 }
